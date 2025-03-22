671b59f51c3e7d71bb53c65e0c6695a560b433bb
bahae112 <bahaeaouanet2004@gmail.com>
2025-03-22 11:12:26 +0100

    Suppression de tout le contenu

--- ReservationVolsProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ReservationVolsProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
